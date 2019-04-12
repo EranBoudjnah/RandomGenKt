0d0cdce216087bf52e78eba9a857a280ca580d0b
Eran Boudjnah <eranbou@gmail.com>
2019-04-12 15:38:36 +0100

    Updated dependency versions.

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -34,7 +34,7 @@ repositories {
 }
 
 dependencies {
-	implementation("androidx.appcompat:appcompat:1.0.0")
+	implementation("androidx.appcompat:appcompat:1.0.2")
 	implementation("androidx.constraintlayout:constraintlayout:1.1.3")
 	implementation("com.google.android.material:material:1.0.0")
 	implementation("androidx.lifecycle:lifecycle-extensions:2.0.0")
@@ -54,14 +54,14 @@ dependencies {
 	testImplementation("junit:junit:4.12")
 	testImplementation("org.hamcrest:hamcrest-core:1.3")
 	testImplementation("org.mockito:mockito-core:2.23.0")
-	testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.0.0-RC3")
+	testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.1.0")
 	testImplementation("android.arch.core:core-testing:1.1.1")
 
-	androidTestImplementation("androidx.test.espresso:espresso-core:3.1.0")
-	androidTestImplementation("androidx.test.espresso:espresso-contrib:3.1.0")
-	androidTestImplementation("androidx.test:runner:1.1.0")
-	androidTestImplementation("androidx.test:rules:1.1.0")
-	androidTestImplementation("androidx.test.ext:junit:1.0.0")
+	androidTestImplementation("androidx.test.espresso:espresso-core:3.1.1")
+	androidTestImplementation("androidx.test.espresso:espresso-contrib:3.1.1")
+	androidTestImplementation("androidx.test:runner:1.1.1")
+	androidTestImplementation("androidx.test:rules:1.1.1")
+	androidTestImplementation("androidx.test.ext:junit:1.1.0")
 
 	implementation("com.mitteloupe:randomgenkt:1.0.0")
 }
