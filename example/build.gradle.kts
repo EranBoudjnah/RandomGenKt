eadde699be410c883214227036151b4f517d00a9
Eran Boudjnah <eranbou@gmail.com>
2019-07-11 00:47:43 +0100

    Updated dependency versions.

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     implementation("com.google.android.material:material:1.0.0")
     implementation("androidx.lifecycle:lifecycle-extensions:2.0.0")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.1.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.0.0")
 
     implementation("com.google.dagger:dagger:2.19")
@@ -57,15 +57,15 @@ dependencies {
 
     testImplementation("junit:junit:4.12")
     testImplementation("org.hamcrest:hamcrest-core:1.3")
-    testImplementation("org.mockito:mockito-core:2.23.0")
+    testImplementation("org.mockito:mockito-core:2.28.2")
     testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.1.0")
     testImplementation("android.arch.core:core-testing:1.1.1")
 
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.1")
-    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.1.1")
-    androidTestImplementation("androidx.test:runner:1.1.1")
-    androidTestImplementation("androidx.test:rules:1.1.1")
-    androidTestImplementation("androidx.test.ext:junit:1.1.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.2.0")
+    androidTestImplementation("androidx.test:runner:1.2.0")
+    androidTestImplementation("androidx.test:rules:1.2.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
 
     implementation("com.mitteloupe:randomgenkt:1.0.0")
 }
--- randomgenkt/build.gradle.kts
+++ randomgenkt/build.gradle.kts
@@ -11,13 +11,13 @@ dependencies {
     ktlint("com.github.shyiko:ktlint:0.31.0")
 
     compile(kotlin("stdlib-jdk8"))
-    compile("org.jetbrains.kotlin:kotlin-reflect:1.3.21")
+    compile("org.jetbrains.kotlin:kotlin-reflect:1.3.41")
 
     implementation("com.implimentz:unsafe:0.0.6")
 
     testImplementation("junit:junit:4.12")
     testImplementation("org.hamcrest:hamcrest-core:1.3")
-    testImplementation("org.mockito:mockito-core:2.23.0")
+    testImplementation("org.mockito:mockito-core:2.28.2")
     testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.1.0")
 }
 
