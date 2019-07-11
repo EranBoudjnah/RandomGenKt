a36f3607d84d3ff52f0476116bb2b0437c71cb92
Eran Boudjnah <eranbou@gmail.com>
2019-07-11 01:09:18 +0100

    Updated dependencies Updated travis file

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,8 @@ buildscript {
 		mavenCentral()
 	}
 	dependencies {
-		classpath("com.android.tools.build:gradle:3.3.2")
-		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.21")
+		classpath("com.android.tools.build:gradle:3.4.1")
+		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.41")
 	}
 }
 
