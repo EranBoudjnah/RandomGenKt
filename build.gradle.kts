70a73beec8714fef8ae9b2a821bacd6211e83cc4
eranboudjnah <shadowcra@gmail.com>
2019-12-07 11:33:31 +0000

    Updated build tools and gradle plugin versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,8 @@ buildscript {
 		mavenCentral()
 	}
 	dependencies {
-		classpath("com.android.tools.build:gradle:3.4.1")
-		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.41")
+		classpath("com.android.tools.build:gradle:3.5.3")
+		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
 	}
 }
 
